4365124b26efb1f6168091650f1290f685b32dba
Hermon Jay <hermon_jay@yahoo.com>
2018-07-12 15:07:12 +0700

    fix grade range

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 def calculate_gpa(total_SKS, total_bobot):
     return total_bobot / total_SKS
 
+
 def count_per_grade(dataFrame):
     a = 0
     a_min = 0
@@ -12,24 +13,41 @@ def count_per_grade(dataFrame):
     c_plus = 0
     c = 0
     c_min = 0
+    c_per_d = 0
+    d_plus = 0
     d = 0
     e = 0
     for i in range(len(dataFrame['NILAI'])):
-        if dataFrame['NILAI'][i] == 'A': a += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'A-': a_min += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] =='A/B': a_per_b += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'B+': b_plus += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'B': b += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'B-': b_min += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'B/C': b_per_c += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'C+': c_plus += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'C': c += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'C-': c_min += dataFrame['SKS'][i]
-        elif dataFrame['NILAI'][i] == 'D': d += dataFrame['SKS'][i]
-        else: e += dataFrame['SKS'][i]    
-        
+        if dataFrame['NILAI'][i] == 'A':
+            a += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'A-':
+            a_min += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'A/B':
+            a_per_b += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'B+':
+            b_plus += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'B':
+            b += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'B-':
+            b_min += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'B/C':
+            b_per_c += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'C+':
+            c_plus += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'C':
+            c += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'C-':
+            c_min += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'C/D':
+            c_per_d += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'D+':
+            d_plus += dataFrame['SKS'][i]
+        elif dataFrame['NILAI'][i] == 'D':
+            d += dataFrame['SKS'][i]
+        else:
+            e += dataFrame['SKS'][i]
+
     print('Nilai A: %d \tNilai A-: %d \tNilai A/B: %d \tNilai B+: %d' % (a, a_min, a_per_b, b_plus))
     print('Nilai B: %d \tNilai B-: %d \tNilai B/C: %d \tNilai C+: %d' % (b, b_min, b_per_c, c_plus))
-    print('Nilai C: %d \tNilai C-: %d \tNilai D  : %d \tNilai E : %d' % (c, c_min, d, e))
-
-    
+    print('Nilai C: %d \tNilai C-: %d \tNilai C/D: %d \tNilai D+: %d' % (c, c_min, c_per_d, d_plus))
+    print('Nilai D: %d \tNilai E : %d' % (d, e))
